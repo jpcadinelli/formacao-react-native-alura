d88e93d669873aab111e0881d7495b26203acba0
Joao Pedro Cadinelli <jpcadinelli@gmail.com>
2023-10-24 10:42:04 -0300

    adicionando hook de redução

--- curso-React-Native-utilizando-e-criando-Hooks/orgsHooks/src/telas/Home/componentes/Produtor.js
+++ curso-React-Native-utilizando-e-criando-Hooks/orgsHooks/src/telas/Home/componentes/Produtor.js
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { Text, View, Image, StyleSheet, TouchableOpacity } from "react-native";
 import Estrelas from "../../../componentes/Estrelas";
 
 export default function Produtor({ nome, imagem, distancia, estrelas }) {
-    const [selecionado, setSelecionado] = useState(false);
+    const [selecionado, inverterSelecionado] = useReducer(
+            (selecionado) => !selecionado,
+            false
+        );
 
     return <TouchableOpacity 
             style={estilos.cartao}
-            onPress={() => setSelecionado(!selecionado)}
+            onPress={inverterSelecionado}
         >
             <Image source={imagem} accessibilityLabel={nome} style={estilos.imagem} />
             <View style={estilos.informacoes} >
